dafbc7d66efea1ee1c546b7bb40987078a97e918
Frank-003 <oyiengofrankline49@gmail.com>
2024-04-20 01:47:11 +0000

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,4 @@
-The code that will create a file name called school inside /tmp directory
+#The code that will create a file name called school inside /tmp directory
 file { '/tmp/school':
 ensure => file,
 content => 'I love Puppet',
