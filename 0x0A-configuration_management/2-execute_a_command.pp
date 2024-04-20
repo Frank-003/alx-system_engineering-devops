db4c68fd1b213d20c68ded127aed4ab2fe0a7bea
Frank-003 <oyiengofrankline49@gmail.com>
2024-04-20 02:11:26 +0000

    task 2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+#This code kills a process && works together with the killnow file which has already been prvided
+exec { killmenow':
+  command  =>  '/usr/bin/pkill killmenow',
+  provider =>  'shell',
+  returns  =>  [0, 1],
+}
