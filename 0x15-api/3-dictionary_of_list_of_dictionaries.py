c01048a08b288c2e7fb1857a211019e6fc1a5e05
Frank-003 <oyiengofrankline49@gmail.com>
2024-05-20 06:13:35 -0700

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
-import json
-import requests
-
-def fetch_user_data(user_id):
-    """
-    Fetch user data and tasks for a given user ID, then export them to a JSON file.
-
-    :param user_id: ID of the user to fetch data for
-    """
-    # Fetch user information
-    user_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{user_id}")
-    user_response.raise_for_status()
-    user_data = user_response.json()
-    username = user_data['username']
-
-    # Fetch user's tasks
-    tasks_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{user_id}/todos")
-    tasks_response.raise_for_status()
-    tasks_data = tasks_response.json()
-
-    # Prepare the data to be exported
-    tasks_list = [{
-        "task": task['title'],
-        "completed": task['completed'],
-        "username": username
-    } for task in tasks_data]
-
-    # Prepare the final data structure
-    data = {str(user_id): tasks_list}
-
-    # Export to JSON file
-    file_name = f"{user_id}.json"
-    with open(file_name, "w") as json_file:
-        json.dump(data, json_file, indent=4)
-
-    print(f"Data for user {user_id} has been exported to {file_name}")
+"""Exports data in the JSON format"""
 
 if __name__ == "__main__":
-    user_id = 1  # Example user_id, replace with the desired user_id
-    fetch_user_data(user_id)
+
+    import json
+    import requests
+    import sys
+
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
+
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
 
