eb1af52e9f3b29d8b23d6fd093d7d4c36e04c980
Frank-003 <oyiengofrankline49@gmail.com>
2024-06-04 14:10:30 +0000

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-import praw
+"""Module for task 1"""
+
 
 def top_ten(subreddit):
-    try:
-        reddit = praw.Reddit(client_id='your_client_id', client_secret='your_client_secret', user_agent='your_user_agent')
-        posts = reddit.subreddit(subreddit).hot(limit=10)
-        for post in posts:
-            print(post.title)
-    except praw.exceptions.Redirect:
-        print("None") 
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
