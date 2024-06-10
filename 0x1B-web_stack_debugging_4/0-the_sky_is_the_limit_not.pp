16b100d26cdf08041eea7881f9f3aa6be410adbf
Frank-003 <oyiengofrankline49@gmail.com>
2024-06-10 07:59:35 +0000

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,16 @@
+http {
+    ...
+    proxy_cache_path /var/cache/nginx levels=1:2 keys_zone=my_cache:10m max_size=10g inactive=60m use_temp_path=off;
+
+    server {
+        ...
+        location / {
+            proxy_cache my_cache;
+            proxy_pass http://localhost:your_app_port;
+            proxy_cache_valid 200 302 10m;
+            proxy_cache_valid 404 1m;
+            proxy_cache_bypass $http_cache_control;
+        }
+    }
+}
+
